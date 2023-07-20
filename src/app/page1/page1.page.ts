a4aa90d6f6db4aebe74f3cb9a658b811d68e9997
ikyonic <jeraldnino.cardenas.xentra@gmail.com>
2023-07-21 01:44:15 +0800

    page1 and page2

--- src/app/page1/page1.page.ts
+++ src/app/page1/page1.page.ts
@@ -6,36 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./page1.page.scss'],
 })
 export class Page1Page implements OnInit {
-  public actionSheetButtons = [
-    {
-      text: 'Delete',
-      role: 'destructive',
-      data: {
-        action: 'form',
-        
-      },
-    },
-    {
-      text: 'Share',
-      data: {
-        action: 'share',
-      },
-    },
-    {
-      text: 'Cancel',
-      role: 'cancel',
-      data: {
-        action: 'cancel',
-      },
-    },
-    {
-      text: 'Cancel',
-      role: 'destructive',
-      data: {
-        action: 'cancel',
-      },
-    },
-  ];
+
 
   constructor() { }
 
