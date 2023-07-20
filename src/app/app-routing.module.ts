a2d28650555fb941fb86c91fff63bb56ea5c0e5b
ikyonic <jeraldnino.cardenas.xentra@gmail.com>
2023-07-20 23:03:27 +0800

    New Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,15 @@ const routes: Routes = [
   {
     path: 'page1',
     loadChildren: () => import('./page1/page1.module').then( m => m.Page1PageModule)
+  },  {
+    path: 'page2',
+    loadChildren: () => import('./page2/page2.module').then( m => m.Page2PageModule)
   },
+  {
+    path: 'page3',
+    loadChildren: () => import('./page3/page3.module').then( m => m.Page3PageModule)
+  },
+
 ];
 
 @NgModule({
